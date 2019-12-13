5b471b5dd222ec2f6a561ff7e0e28b93687187e9
Drazen Bebic <drazen.bebic@outlook.com>
2019-12-13 13:37:52 +0100

    Fix header component

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -3,7 +3,9 @@ import "./header.module.scss"
 
 export default props => (
   <header>
-    <h1>License Manager for WooCommerce</h1>
-    <p>Easily sell and manage software license keys through your shop</p>
+    <div>
+      <h1>License Manager for WooCommerce</h1>
+      <p>Easily sell and manage software license keys through your shop</p>
+    </div>
   </header>
 )
