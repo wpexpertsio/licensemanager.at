e8e10411703df08a9b6e90073b0525245c84e058
Drazen Bebic <drazen.bebic@outlook.com>
2019-12-15 02:57:57 +0100

    Add Font Awesome

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,1 +1,2 @@
 import "./src/styles/global.css"
+import "./src/styles/fontawesome/css/all.min.css"
--- src/pages/index.js
+++ src/pages/index.js
@@ -26,40 +26,34 @@ export default () => (
 
     <div className="row">
       <div className="col-12">
-        VIDEO
-      </div>
-    </div>
-
-    <div className="row">
-      <div className="col-12">
         <Heading>Feature overview</Heading>
       </div>
     </div>
 
     <div className="row">
       <div className="col-6">
-        <h3>Import your license keys</h3>
+        <h3><i className="fas fa-file-import"/> Import your license keys</h3>
         <p>Add and import your license keys to your WooCommerce shop. You can add them one by one, or in bulk! Just
           upload your CSV/TXT file and be done with it. Sell your added keys, use a generator, or… do both!</p>
       </div>
       <div className="col-6">
-        <h3>Generate license keys</h3>
+        <h3><i className="fas fa-cog" />Generate license keys</h3>
         <p>Do you need to generate license keys on-the-go with each new order? No problem! Enable the option and let
           the plugin do the work for you. Create highly customizable generators, so that your license key looks just
           the way it you need it to.</p>
       </div>
       <div className="col-6">
-        <h3>Automatic delivery</h3>
+        <h3><i className="fas fa-truck"/>Automatic delivery</h3>
         <p>Automatically deliver your license keys to the customer by simply enabling the feature inside the settings
           page. The individual license keys will be sent out with the standard WooCommerce “Order complete” email.</p>
       </div>
       <div className="col-6">
-        <h3>Encryption</h3>
+        <h3><i className="fas fa-lock"/>Encryption</h3>
         <p>Your license keys are always stored in an encrypted fashion inside the database. This prevents anyone who
           might get access to your database from using your customer’s license keys.</p>
       </div>
       <div className="col-6">
-        <h3>WooCommerce Integration</h3>
+        <h3><i className="fas fa-store"/>WooCommerce Integration</h3>
         <p>The License Manager plugin is built for WooCommerce, thus seamlessly integrates into WooCommerce
           functionality. Offering licensing options for both simple and variable products, adding new and
           customizable emails, new order actions, and much more.</p>
